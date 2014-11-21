8c66e4e1d2c17e4f6f4e5477992b67bbd1ffee86
Matthew Turney <mturney@digium.com>
2014-11-21 12:36:46 -0600

    Asset build task for SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
+'use strict';
+
+// gulp and plugins
 var gulp = require('gulp');
-var deploy = require('gulp-gh-pages');
+var $ = require('gulp-load-plugins')();
+
+var paths = {
+    templates: __dirname + '/templates/',
+    output: __dirname + '/build',
+    input: __dirname + '/src',
+    scss: __dirname + '/src/scss'
+};
 
 gulp.task('deploy', function () {
     var options = {};
     return gulp
         .src('./build/**/*')
-        .pipe(deploy(options));
+        .pipe($.deploy(options));
+});
+
+gulp.task('build:assets', function assetsTask() {
+    var cssOutput = paths.output + '/css';
+
+    return gulp.src(paths.scss + '/**/*')
+        .pipe($.sourcemaps.init())
+            .pipe($.sass({
+                errLogToConsole: true
+            }))
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest(cssOutput));
 });
