79926201c76837854e8b360f8b1545ab928324d9
Tian Davis <tiandavis@gmail.com>
2015-04-19 11:41:19 -0400

    Updated Node.js example to show installing respoke-admin and requiring it.

--- examples/endpoint-authentication.js
+++ examples/endpoint-authentication.js
@@ -1,11 +1,15 @@
+// npm install respoke-admin
+
+var Respoke = require("respoke-admin");
+
 var respoke = new Respoke({
-    appId: 'c10a2075-3f3d-466f-82f9-d2285e64c5d4',
-    'App-Secret': 'eb327e57-e766-49de-b801-ef612a70509e'
+    appId: "c10a2075-3f3d-466f-82f9-d2285e64c5d4",
+    "App-Secret": "eb327e57-e766-49de-b801-ef612a70509e"
 });
 
 respoke.auth.endpoint({
-    endpointId: 'spock@enterprise.com',
-    roleId: '371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F'
+    endpointId: "spock@enterprise.com",
+    roleId: "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F"
 }, function (err, response) {
     res.json({
         token: response.tokenId
